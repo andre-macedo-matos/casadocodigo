a8b038e9f9e9290247ce0a5dd2fe0be9e020503c
Macedo <Macedo@192.168.0.13>
2022-02-21 22:48:41 -0300

    Implementado expor respostas em outros formatos
    Implementado JsonViewResolver para retornar com os produtos no corpo da
    resposta.
    
    Implementado em AppWebConfiguration.contentNegotiatingViewResolver lista
    com as implementações para lidar com o formato da respota (html e json).

--- src/main/java/org/casadocodigo/controllers/ProductsController.java
+++ src/main/java/org/casadocodigo/controllers/ProductsController.java
@@ -73,7 +73,4 @@ public class ProductsController {
 		return modelAndView;
 	}
 	
-	
-	
-	
 }
--- src/main/java/org/casadocodigo/loja/config/AppWebConfiguration.java
+++ src/main/java/org/casadocodigo/loja/config/AppWebConfiguration.java
@@ -1,11 +1,14 @@
 package org.casadocodigo.loja.config;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.casadocodigo.controllers.HomeController;
 import org.casadocodigo.daos.ProductDAO;
 import org.casadocodigo.loja.infra.FileSaver;
 import org.casadocodigo.loja.models.ShoppingCart;
+import org.casadocodigo.loja.viewresolver.JsonViewResolver;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.guava.GuavaCacheManager;
@@ -17,10 +20,13 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.google.common.cache.CacheBuilder;
@@ -80,6 +86,20 @@ public class AppWebConfiguration {
 		cacheManager.setCacheBuilder(builder);
 		
 		return cacheManager;
+	}
+	
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+		
+		resolvers.add(internalResourceViewResolver());
+		resolvers.add(new JsonViewResolver());
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(resolvers);
+		resolver.setContentNegotiationManager(manager);
+		
+		return resolver;
 	}
 	
 }
