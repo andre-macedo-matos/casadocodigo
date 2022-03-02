a2b4f460c8e6e4c122e98d1731b05f5b53068757
Macedo <Macedo@192.168.0.13>
2022-03-02 19:24:24 -0300

    Solução para evitar Lazy Load
    Implementado filtro em ServletSpringMVC.getServletFilters para permitir
    que o EntityManager esteja sempre aberto durante o ciclo da requisição.
    Assim evitamos de fazer N + 1 consultas sempre que um get é disparado em
    alguima .jsp

--- src/main/java/org/casadocodigo/loja/config/ServletSpringMVC.java
+++ src/main/java/org/casadocodigo/loja/config/ServletSpringMVC.java
@@ -2,11 +2,13 @@ package org.casadocodigo.loja.config;
 
 
 
+import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
@@ -38,6 +40,13 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 		
 		servletContext.addListener(RequestContextListener.class);
 		servletContext.setInitParameter("spring.profiles.active", "dev");
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] {
+				new OpenEntityManagerInViewFilter()
+		};
 	}
 
 }
