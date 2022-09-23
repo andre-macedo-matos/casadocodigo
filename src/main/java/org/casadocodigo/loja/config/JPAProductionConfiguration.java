0b7977114962c2e72564b5853eea92950076c109
andre-macedo-matos <andremacedomatos@gmail.com>
2022-09-23 15:55:51 -0300

    Correct url to connect at production(heroku)

--- src/main/java/org/casadocodigo/loja/config/JPAProductionConfiguration.java
+++ src/main/java/org/casadocodigo/loja/config/JPAProductionConfiguration.java
@@ -5,22 +5,28 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class JPAProductionConfiguration {
 
+	@Autowired
+	Environment env;
+	
 	@Bean
 	@Profile("prod")
 	public DataSource dataSource() throws URISyntaxException{
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		
+
 		dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(System.getenv("JDBC_DATABASE_URL"));
-        dataSource.setUsername(System.getenv("JDBC_DATABASE_USERNAME"));
-        dataSource.setPassword(System.getenv("JDBC_DATABASE_PASSWORD"));
-		
+        dataSource.setUrl     (env.getProperty("JDBC_DATABASE_URL", "jdbc:postgresql://localhost:5432/casadocodigo"));
+        dataSource.setUsername(env.getProperty("JDBC_DATABASE_USERNAME", "postgres"));
+        dataSource.setPassword(env.getProperty("JDBC_DATABASE_PASSWORD", ""));
+        
 		return dataSource;
 	}
 	
