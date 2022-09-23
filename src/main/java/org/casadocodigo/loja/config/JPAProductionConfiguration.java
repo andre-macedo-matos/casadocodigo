c8fdfc5193f0b9ad6f8efe720ef1b9fcefea21c9
andre-macedo-matos <andremacedomatos@gmail.com>
2022-09-23 00:49:44 -0300

    Refactor to get system enviorment variable

--- src/main/java/org/casadocodigo/loja/config/JPAProductionConfiguration.java
+++ src/main/java/org/casadocodigo/loja/config/JPAProductionConfiguration.java
@@ -17,9 +17,9 @@ public class JPAProductionConfiguration {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		
 		dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(System.getProperty("JDBC_DATABASE_URL", "jdbc:postgresql://localhost:5432/casadocodigo"));
-        dataSource.setUsername(System.getProperty("JDBC_DATABASE_USERNAME", "postgres"));
-        dataSource.setPassword(System.getProperty("JDBC_DATABASE_PASSWORD",""));
+        dataSource.setUrl(System.getenv("JDBC_DATABASE_URL"));
+        dataSource.setUsername(System.getenv("JDBC_DATABASE_USERNAME"));
+        dataSource.setPassword(System.getenv("JDBC_DATABASE_PASSWORD"));
 		
 		return dataSource;
 	}
