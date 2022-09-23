58220885afc95fb546f819489777557dffe5bd9c
andre-macedo-matos <andremacedomatos@gmail.com>
2022-09-23 00:36:08 -0300

    Refactor use of system properties

--- src/main/java/org/casadocodigo/loja/config/JPAProductionConfiguration.java
+++ src/main/java/org/casadocodigo/loja/config/JPAProductionConfiguration.java
@@ -5,26 +5,21 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class JPAProductionConfiguration {
 
-	@Autowired
-	private Environment environment;
-	
 	@Bean
 	@Profile("prod")
 	public DataSource dataSource() throws URISyntaxException{
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		
 		dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(environment.getProperty("JDBC_DATABASE_URL", "jdbc:postgresql://localhost:5432/casadocodigo"));
-        dataSource.setUsername(environment.getProperty("JDBC_DATABASE_USERNAME", "postgres"));
-        dataSource.setPassword(environment.getProperty("JDBC_DATABASE_PASSWORD",""));
+        dataSource.setUrl(System.getProperty("JDBC_DATABASE_URL", "jdbc:postgresql://localhost:5432/casadocodigo"));
+        dataSource.setUsername(System.getProperty("JDBC_DATABASE_USERNAME", "postgres"));
+        dataSource.setPassword(System.getProperty("JDBC_DATABASE_PASSWORD",""));
 		
 		return dataSource;
 	}
